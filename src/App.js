4f687e7bc9ace6e127f9843138fdd33461bd33a9
bonface221 <mainaboniface221@gmail.com>
2022-12-22 00:22:52 +0300

    ft:added the search functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useState } from 'react';
+import { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			monsters: [],
+			input: "",
+		};
+	}
+	url = "https://jsonplaceholder.typicode.com/users";
+
+	componentDidMount() {
+		fetch(this.url)
+			.then((res) => res.json())
+			.then((users) =>
+				this.setState(
+					() => ({ monsters: users }),
+					() => console.log(this.state)
+				)
+			);
+	}
+
+	render() {
+		let myData;
+		if (!this.state.input) {
+			myData = this.state.monsters;
+		} else {
+			myData = this.state.monsters.filter(({ name }) =>
+				name.includes(this.state.input)
+			);
+		}
+		return (
+			<div className="App">
+				<p>search for a monster</p>
+				<input
+					className="search-box"
+					type="search"
+					placeholder="search monsters"
+					onChange={(e) =>
+						this.setState({
+							input: e.target.value,
+						})
+					}
+				/>
+				<ul>
+					{myData.map((monster, index) => (
+						<li className="myLi" key={index}>
+							{monster.name}
+						</li>
+					))}
+				</ul>
+			</div>
+		);
+	}
 }
 
 export default App;
